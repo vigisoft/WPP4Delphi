343fb122c348ba02c54216836643ea46aacfccda
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2023-09-05 17:12:15 -0300

    Added title translation for QR-Code close box

--- Source/Model/uTWPPConnect.Constant.pas
+++ Source/Model/uTWPPConnect.Constant.pas
@@ -861,6 +861,7 @@ resourcestring
   Text_FrmQRCode_CaptionStart          = '';
   Text_FrmQRCode_CaptionSucess         = '';
   Text_FrmQRCode_OnCLose               = '';
+  Text_FrmQRCode_Caption               = '';
   MSG_ExceptPhoneNumberError           = '';
   MSG_ExceptAlterDesigner              = '';
   MSG_ExceptCOntactNotFound            = '';
@@ -926,22 +927,6 @@ resourcestring
   Text_FrmConfigNetWork_BntCancel        = '';
   Text_FrmConfigNetWork_QuestionSave     = '';
   Text_FrmConfigNetWork_PrtocolLbl       = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-//  Text_FrmQRCode_OnCLose                 = '';
-
 type
 
     TLanguageInject       = (TL_Portugues_BR=0, TL_English=1, TL_Espanol=2, TL_Farsi=3, TL_Italian=4, TL_German=5, TL_French=6,
